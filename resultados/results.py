c6f1c92aaedc5911b4e9ea4f8f95d1e54f5fda40
JuaniTollo <juantollo@gmail.com>
2024-05-29 20:27:06 -0300

    Arreglo visualizacion learning rate data frame

--- resultados/results.py
+++ resultados/results.py
@@ -4,7 +4,6 @@ import pandas as pd
 import mlx.core as mx
 import mlx.nn as nn
 import numpy as np
-from sklearn.metrics import accuracy_score
 from utils import *
 import os
 from pathlib import Path
@@ -50,72 +49,46 @@ def parse_path(path):
             "Dataset": parts[5],  # El quinto elemento es el dataset
             "Capas LoRa": parts[6],  # El sexto elemento son las capas LoRa
             "Tamaño batch": parts[7],  # El séptimo elemento es el tamaño del batch
-            "Iters": parts[8], 
-            "Learning rate": str(parts[9]),  # El octavo elemento es la tasa de aprendizaje
+            "Iters": parts[8],
+            "Lora_Rank": 8, 
+            "Learning rate": f"{float(parts[9]):.1e}",  # El octavo elemento es la tasa de aprendizaje
             "Base/Fine-tuning": "Fine-tuning",
             "Adapter": "Best"
             }
+    
     else:
-        
+        #
         info = {
             "Empresa": parts[3],  # El tercer elemento es la empresa
             "Modelo": parts[4],   # El cuarto elemento es el modelo
             "Dataset": parts[5],  # El quinto elemento es el dataset
             "Capas LoRa": "n/a",  # El sexto elemento son las capas LoRa
             "Tamaño batch": "n/a",  # El séptimo elemento es el tamaño del batch
-            "Learning rate": "n/a",  # El octavo elemento es la tasa de aprendizaje
-            "Iters":parts[7],
+            "Learning rate": "0",  # El octavo elemento es la tasa de aprendizaje
+            "Iters":"n/a",
             "Base/Fine-tuning": "Base",
             "Adapter": "n/a"
 }               
-    #pdb.set_trace()
+    
     return info
 def cargar_logits_y_targets(dir):
     logits = np.load(dir + "/test_all_logits.npy")
     targets = np.load(dir + "/test_all_targets.npy")
     return logits, targets
 
-def calcular_metricas(logits,targets):
-    # Si ambos archivos existen, procede con el procesamiento
-        np.bincount(targets, minlength=3)
-        
-        values, counts = np.unique(targets, return_counts=True)
-        counts / sum(counts)
-
-        predictions = np.argmax(logits, axis=1)
-        
-        accuracy = accuracy_score(targets, predictions)
-
-        logits_sf = mx.softmax(mx.array(logits), axis=1)
-
-        indices = np.unique(targets)
-        selected_logits = np.take(logits_sf, indices, axis=1)
-        priors_to_targets = np.sum(selected_logits, axis=1).mean()
-
-        relevant_logits = logits[:, indices]
-        predictions = np.argmax(relevant_logits, axis=1)
-
-        index_to_position = {index: pos for pos, index in enumerate(indices)}
-        mapped_targets = np.array([index_to_position[target] for target in targets if target in index_to_position])
-        accuracy_restricted = accuracy_score(mapped_targets, predictions)
-
-        cross_entropy = nn.losses.cross_entropy(mx.softmax(mx.array(logits)), mx.array(targets), reduction="mean").item()
-        epsilon = 1e-10  # Añadir epsilon para evitar el log de cero
-        priors = np.bincount(targets, minlength=logits.shape[1]) / targets.shape[0]
-        priors *= epsilon
-        priors = np.repeat(priors.reshape(1, -1), len(targets), axis=0)
-
-        priors = np.clip(priors, a_min=1e-10, a_max=None)  # Ensure all values are >= 1e-10
-        cross_entropy_priors = nn.losses.cross_entropy(mx.array(np.log(priors)), mx.array(targets), reduction="mean").item()
-        
-        return accuracy, accuracy_restricted, priors_to_targets,cross_entropy,cross_entropy_priors
+def clean_and_concat(df_main, df_temp):
+    # Limpiar columnas con todos valores NA
+    df_temp_cleaned = df_temp.dropna(how='all', axis=1)
+    # Concatenar solo si df_temp_cleaned no está vacío
+    if not df_temp_cleaned.empty:
+        return pd.concat([df_main, df_temp_cleaned], ignore_index=True)
+    return df_main
 
 # Initialize a DataFrame to hold all results
 def iniciailizarDataSet():
     results_df = pd.DataFrame(columns=["Empresa","Modelo", "Dataset","Capas LoRa", "Tamaño batch", "Iters", "Learning rate","Adapter", "Accuracy", "Proportion Targets in Softmax", "Accuracy restringido", "Entropía cruzada","Entropía cruzada normalizada"])
     results_df['Learning rate'] = results_df['Learning rate'].astype(str)
     return results_df
-
 
 results_df = iniciailizarDataSet()
 
@@ -124,61 +97,48 @@ ignore_folders = ['adapters', 'adapters_best_val', 'tokenizer', 'resultados con 
 
 leaf_dirs = list_leaf_directories(root_directory, ignore_folders)
 
-
 for dir in leaf_dirs[:]:
     try:
-        #print("Procesando directorio:", dir)
-        try:
-            info_experimento = parse_path(dir)
-        except:
-            print(f"No se pudo parsear {dir}")
-
-        try:
-            logits, targets = cargar_logits_y_targets(dir)
-        except:
-            print(f"No se pudo encontrar 'test_all_logits.npy' o 'test_all_targets.npy' en {dir}, saltando este directorio.")
-            continue
-        accuracy, accuracy_restricted, priors_to_targets,cross_entropy,cross_entropy_priors = calcular_metricas(logits,targets)
+        prefixes = ["train", "val", "test"]
+        print(dir)
+        resultados = calcular_metricas_para_conjuntos(dir, prefixes)
+        info_experimento = parse_path(dir)
+    except:
+        print(f"No se pudo parsear {dir}")
 
         # Preparar un DataFrame temporal con los resultados para este directorio
-        temp_df = pd.DataFrame({
-            "Empresa": info_experimento["Empresa"],
-            "Dataset": info_experimento["Dataset"],     
-            "Modelo": info_experimento["Modelo"],
-            "Base/Fine-tuning": info_experimento["Base/Fine-tuning"],
-            "Capas LoRa": info_experimento["Capas LoRa"],
-            "Tamaño batch": info_experimento["Tamaño batch"],
-            "Learning rate": info_experimento["Learning rate"],
-            "Iters": info_experimento["Iters"],
-            "Adapter": info_experimento["Adapter"],
-            "Accuracy": [accuracy],
-            "Proportion Targets in Softmax": [priors_to_targets],
-            "Accuracy restringido": [accuracy_restricted],
-            "Entropía cruzada": [cross_entropy],
-            "Entropía cruzada normalizada": [cross_entropy / cross_entropy_priors],
-        })
-        temp_df['Learning rate'].astype(str)
-
-        # Concatenar el DataFrame temporal al principal
-        #pdb.set_trace()
-        if not results_df.dropna(how='all', axis=1).empty:
-            results_df = pd.concat([results_df, temp_df], ignore_index=True)
-        else:
-            results_df = temp_df
-        
-        
-    except Exception as e:
-        print(f"Ocurrió un error inesperado procesando el directorio {dir}: {str(e)}")
+    # Crear el DataFrame incluyendo métricas para train, val y test
+   
+    temp_df = pd.DataFrame({
+        "Empresa": info_experimento["Empresa"],
+        "Dataset": info_experimento["Dataset"],
+        "Modelo": info_experimento["Modelo"],
+        "Base/Fine-tuning": info_experimento["Base/Fine-tuning"],
+        "Capas LoRa": info_experimento["Capas LoRa"],
+        "Tamaño batch": info_experimento["Tamaño batch"],
+        "Learning rate": info_experimento['Learning rate'],
+        "Iters": info_experimento["Iters"],
+        "Adapter": info_experimento["Adapter"],
+        "Accuracy Entrenamiento": [resultados.get("accuracy_restricted_train", None)],
+        "Accuracy Validación": [resultados.get("accuracy_restricted_val", None)],
+        "Accuracy Test": [resultados.get("accuracy_restricted_test", None)],
+        "EC Entrenamiento": [resultados.get("cross_entropy_train", None)],
+        "EC Validación": [resultados.get("cross_entropy_val", None)],
+        "EC Testeo": [resultados.get("cross_entropy_test", None)],
+        "ECN Entrenamiento": [resultados.get("cross_entropy_train", None) / resultados.get("cross_entropy_priors_train", 1) if resultados.get("cross_entropy_priors_train") else None],
+        "ECN Validación": [resultados.get("cross_entropy_val", None) / resultados.get("cross_entropy_priors_val", 1) if resultados.get("cross_entropy_priors_val") else None],
+        "ECN Testeo": [resultados.get("cross_entropy_test", None) / resultados.get("cross_entropy_priors_test", 1) if resultados.get("cross_entropy_priors_test") else None],
+    })
+    if not results_df.dropna(how='all', axis=1).empty:
+        #results_df = pd.concat([results_df, temp_df], ignore_index=True)
+        # Usar la función para limpiar y concatenar
+        results_df = clean_and_concat(results_df, temp_df)
     
-    #pdb.set_trace()
-    
-results_df.to_csv("results_df.csv")
+    else:
+        results_df = temp_df
 
-        
-results_df.to_csv("results.csv")
-pd.set_option('display.max_columns', 10)  # Máximo número de columnas a mostrar
-pd.set_option('display.max_rows', 10)     # Máximo número de filas a mostrar
-pd.set_option('display.width', 100)       # Ajustar la anchura para la visualización del DataFrame
-pd.set_option('display.max_colwidth', 20) # Máximo ancho de las columnas
+# Aplicar notación científica a la columna 'Learning rate'
+results_df.to_csv("results_df.csv", index=False)
+
         
     
--- resultados/utils.py
+++ resultados/utils.py
@@ -1,3 +1,10 @@
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+import os
+import mlx.nn as nn
+import mlx.core as mx
+import pdb
 
 def is_leaf_directory(path, ignore_dirs=None):
     """ Verifica si un directorio es un directorio hoja, ignorando ciertos subdirectorios. """
@@ -24,3 +31,77 @@ def list_leaf_directories(root_dir, ignore_dirs=None):
         if is_leaf_directory(root, ignore_dirs):
             leaf_directories.append(root)
     return leaf_directories
+
+# Función para cargar logits y targets
+def cargar_logits_y_targets(dir, prefix):
+    logits_path = f"{dir}/{prefix}_all_logits.npy"
+    targets_path = f"{dir}/{prefix}_all_targets.npy"
+    try:
+        logits = np.load(logits_path)
+        targets = np.load(targets_path)
+        return logits, targets
+    except FileNotFoundError:
+        #print(f"No se pudo encontrar '{logits_path}' o '{targets_path}', saltando este conjunto de datos.")
+        return None, None
+
+# Función para calcular métricas (Ejemplo)
+def calcular_metricas(logits, targets):
+    
+    np.bincount(targets, minlength=3)
+    values, counts = np.unique(targets, return_counts=True)
+    counts / sum(counts)
+
+    predictions = np.argmax(logits, axis=1)
+    
+    accuracy = accuracy_score(targets, predictions)
+
+    logits_sf = mx.softmax(mx.array(logits), axis=1)
+
+    indices = np.unique(targets)
+    selected_logits = np.take(logits_sf, indices, axis=1)
+    # Uso esta a continuacion para la CE normalizada
+    priors_to_targets = np.sum(selected_logits, axis=1).mean()
+
+    relevant_logits = logits[:, indices]
+    predictions = np.argmax(relevant_logits, axis=1)
+
+    index_to_position = {index: pos for pos, index in enumerate(indices)}
+    mapped_targets = np.array([index_to_position[target] for target in targets if target in index_to_position])
+    accuracy_restricted = accuracy_score(mapped_targets, predictions)
+    cross_entropy = nn.losses.cross_entropy(mx.array(logits), mx.array(targets), reduction="mean").item()
+    epsilon = 1e-10  # Añadir epsilon para evitar el log de cero
+    priors = np.bincount(targets, minlength=logits.shape[1]) / targets.shape[0]
+    
+    # TODO: Volar epsilon y recalcular crosentropy
+    priors *= epsilon
+    priors = np.repeat(priors.reshape(1, -1), len(targets), axis=0)
+
+    priors = np.clip(priors, a_min=1e-10, a_max=None)  # Ensure all values are >= 1e-10
+    cross_entropy_priors = nn.losses.cross_entropy(mx.array(np.log(priors)), mx.array(targets), reduction="mean").item()
+    accuracy = np.mean(np.argmax(logits, axis=1) == targets)
+    accuracy_restricted = accuracy  # Supón que es una métrica calculada de manera similar
+    
+    return accuracy, accuracy_restricted, priors_to_targets, cross_entropy, cross_entropy_priors
+
+# Función para calcular métricas para cada conjunto de datos
+def calcular_metricas_para_conjuntos(dir, prefixes):
+    resultados = {}
+    for prefix in prefixes:
+        try:
+            logits, targets = cargar_logits_y_targets(dir, prefix)
+        except:
+            print(f"no se pudo cargar el dataset de {dir}")
+        if logits is None or targets is None:
+            print("logits or targets están vacíos")
+            continue
+        
+        try:
+            metrics = calcular_metricas(logits, targets)
+            metric_names = ["accuracy", "accuracy_restricted", "priors_to_targets", "cross_entropy", "cross_entropy_priors"]
+        except:
+            print("no se pudieron computar las metricas")
+        for name, value in zip(metric_names, metrics):
+            resultados[f"{name}_{prefix}"] = value
+    
+    return resultados
+
